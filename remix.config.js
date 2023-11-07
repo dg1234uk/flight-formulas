1f50879fa7899fa6e7c89177240a7cd52dbb3e13
Daniel Granger <dg1234uk@me.com>
2023-11-07 18:15:09 +0000

    remix-flat-routes added, autoprefixer added

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,21 @@
+import { flatRoutes } from "remix-flat-routes";
+
 /** @type {import('@remix-run/dev').AppConfig} */
+
 export default {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes, {
+      ignoredRouteFiles: [
+        ".*",
+        "**/*.css",
+        "**/*.test.{js,jsx,ts,tsx}",
+        "**/__*.*",
+      ],
+    });
+  },
 };
