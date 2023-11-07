7db8718cb66df4a98c36f675fc51ce46eb5a3b1b
Daniel Granger <dg1234uk@me.com>
2023-11-07 18:16:13 +0000

    Remove commented out code in remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -4,10 +4,6 @@ import { flatRoutes } from "remix-flat-routes";
 
 export default {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
   routes: async (defineRoutes) => {
     return flatRoutes("routes", defineRoutes, {
       ignoredRouteFiles: [
