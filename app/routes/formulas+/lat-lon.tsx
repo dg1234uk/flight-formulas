1bd43da13f716870356c8f8a39039187476d5a03
Daniel Granger <dg1234uk@me.com>
2023-11-15 20:55:45 +0000

    Add H4 heading to LatLon component and update latitude/longitude display

--- app/routes/formulas+/lat-lon.tsx
+++ app/routes/formulas+/lat-lon.tsx
@@ -19,7 +19,7 @@ import {
   FormMessage,
 } from "~/components/ui/form";
 import { useForm } from "react-hook-form";
-import { H3, P } from "~/components/ui/prose";
+import { H3, H4, P } from "~/components/ui/prose";
 import { convertCoordStringToDecimalDegrees } from "~/utils/formulas/latlon";
 
 const latLonFormSchema = z.object({
@@ -54,29 +54,6 @@ export default function LatLon() {
       setLonDd(undefined);
     }
   }
-
-  const latLonMathML = `
-  <math display="block" xmlns="http://www.w3.org/1998/Math/MathML">
-  <mi>Turn Radius</mi>
-  <mo>=</mo>
-  <mfrac>
-    <msup>
-      <mi>V</mi>
-      <mn>2</mn>
-    </msup>
-    <mrow>
-      <mi>g</mi>
-      <mo>&#x22C5;</mo>
-      <mi>tan</mi>
-      <mo fence="true">(</mo>
-        <mrow>
-          <mi>Bank Angle</mi>
-        </mrow>
-        <mo fence="true">)</mo>
-    </mrow>
-  </mfrac>
-</math>
-`;
 
   return (
     <>
@@ -137,7 +114,7 @@ export default function LatLon() {
               </span>
             </p>
             <p className="text-blue-500 dark:text-blue-300">
-              {latDd ?? "N/A"} {lonDd ?? "N/A"}
+              {latDd ?? null} {lonDd ?? null}
             </p>
           </div>
         </CardFooter>
@@ -148,12 +125,42 @@ export default function LatLon() {
             <H3 className="mt-8">
               How the Latitude and Longitude Converter Works
             </H3>
-            <P>Lat Lon all day long</P>
-            <P>The formula for converting</P>
-            <div
-              className="mt-2"
-              dangerouslySetInnerHTML={{ __html: latLonMathML }}
-            />
+            <P>
+              Most digital systems will store latitude and longitude in Decimal
+              Degrees (i.e. 53.3535). With a postive number representing North
+              in latitude and East in longitude. A negative number is used to
+              represent South in latitude and West in longitude
+            </P>
+            <H4 className="mt-4">Converting to Decimal Degrees (D.d)</H4>
+            <P>
+              DMS is often written as degrees(°), minutes ('), and seconds (").
+              There are 60 minutes in a degree and 60 seconds in a minute.
+              Therefore, there are 3600 seconds in a degree (60 x 60 = 3600). Be
+              cautious of the latitude or longitude wrapping around 90° for
+              latitude and 180° for longitude.
+            </P>
+            <P>For example 53° 51' 29.1234"</P>
+            <P>
+              <strong>Step 1</strong>: Identify the degrees, minutes, and
+              seconds. For the above example, degrees = 53, minutes = 51,
+              seconds = 29.1234
+            </P>
+            <P>
+              <strong>Step 2</strong>: Convert the minutes to degrees: Divide
+              the minutes by 60. (51' ÷ 60 = 0.85°)
+            </P>
+            <P>
+              <strong>Step 3</strong>: Convert the seconds to degrees: Divide
+              the seconds by 3600. (29.1234" ÷ 3600 = 0.00808983°)
+            </P>
+            <P>
+              <strong>Step 4</strong>: Add these values to the degrees: 53° +
+              0.85° + 0.00808983° = 53.85808983°
+            </P>
+            <P>
+              If you are converting from degrees and minutes to decimal degrees,
+              then you can skip step 3.
+            </P>
           </article>
         </CardContent>
       </Card>
