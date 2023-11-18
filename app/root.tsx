8e9cd9b179d9fe6788d474021310beb71226dca7
Daniel Granger <dg1234uk@me.com>
2023-11-18 11:49:34 +0000

    Update Remix build tool to Vite

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 import {
   Link,
   Links,
@@ -10,8 +9,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import fontStylesheetUrl from "./styles/mathfonts.css";
-import tailwindStylesheetUrl from "./styles/tailwind.css";
+import "./styles/mathfonts.css";
+import "./styles/tailwind.css";
 import { GeneralErrorBoundary } from "./components/error-boundary";
 import { buttonVariants } from "./components/ui/button";
 
@@ -23,14 +22,6 @@ export const meta: MetaFunction = () => {
       content: `Aviation calculators and unit converters.`,
     },
   ];
-};
-
-export const links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: fontStylesheetUrl },
-    { rel: "stylesheet", href: tailwindStylesheetUrl },
-    ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  ].filter(Boolean);
 };
 
 type Formula = {
@@ -322,12 +313,12 @@ export default function App() {
                       isPending
                         ? "pending"
                         : isActive
-                        ? `${buttonVariants({
-                            variant: "link",
-                          })} bg-blue-200 dark:bg-blue-700`
-                        : `${buttonVariants({
-                            variant: "link",
-                          })}`
+                          ? `${buttonVariants({
+                              variant: "link",
+                            })} bg-blue-200 dark:bg-blue-700`
+                          : `${buttonVariants({
+                              variant: "link",
+                            })}`
                     }
                   >
                     {formula.name}
